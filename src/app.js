5fd533bf274c09294e02afe3d9ec112127bed3a4
naledimuhauli <naledimuhauli@gmail.com>
2023-09-16 14:00:41 +0200

    added a JS structure

---
+++ src/app.js
@@ -0,0 +1,43 @@
+function formatTime(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}}`;
+  }
+  let mintues = date.getMinutes();
+  if (mintues < 10) {
+    mintues = `0${mintues}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day}, ${hours}:${mintues}`;
+}
+
+function getCurrentTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temp");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElemnt = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#currentDate");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElemnt.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = response.data.wind.speed;
+  dateElement.innerHTML = formatTime(response.data.dt * 1000);
+}
+
+let apiKey = "d7968ba5c334606078d89f51a7ec07e6";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(getCurrentTemperature);
