14a3c2a2077716c051ad36970a7a28b811621dfa
naledimuhauli <naledimuhauli@gmail.com>
2023-09-21 22:16:11 +0200

    removed the HTL forecast structure and replaced it with the JS structure

--- src/app.js
+++ src/app.js
@@ -20,6 +20,31 @@ function formatTime(timestamp) {
   let day = days[date.getDay()];
   return `${day}, ${hours}:${mintues}`;
 }
+function displayForecast() {
+  let forecastHTML = `<div class="row">`;
+
+  let forecastElement = document.querySelector("#forecast");
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `<div class="col-2">
+                <div class="weather-forecast-date">${day}</div>
+                <img
+                  src="http:openweathermap.org/img/wn/50d@2x.png"
+                  alt=""
+                  width="50"
+                />
+                <div class="weather-forecast-temperatures">
+                  <span class="weather-forecast-max">18°</span>
+                  <span class="weather-forecast-min">12°</span>
+                </div>
+              </div>
+  `;
+  });
+  forecastHTML = forecastHTML + `</div`;
+  forecastElement.innerHTML = forecastHTML;
+}
 
 function getCurrentTemperature(response) {
   let temperatureElement = document.querySelector("#temp");
@@ -91,3 +116,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("New York");
+displayForecast();
