eacf60ce1b0cff11f2921de957252e6af9d47488
naledimuhauli <naledimuhauli@gmail.com>
2023-09-22 22:36:38 +0200

    displayed the weather forecast data using API

--- src/app.js
+++ src/app.js
@@ -20,30 +20,53 @@ function formatTime(timestamp) {
   let day = days[date.getDay()];
   return `${day}, ${hours}:${mintues}`;
 }
-function displayForecast() {
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+function displayForecast(response) {
+  let forecast = response.data.daily;
+  let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
 
-  let forecastElement = document.querySelector("#forecast");
-  let days = ["Thu", "Fri", "Sat", "Sun"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `<div class="col-2">
-                <div class="weather-forecast-date">${day}</div>
+  forecast.forEach(function (forecastday, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `<div class="col-2">
+                <div class="weather-forecast-date">${formatDay(
+                  forecastday.dt
+                )}</div>
                 <img
-                  src="http:openweathermap.org/img/wn/50d@2x.png"
+                  src="http:openweathermap.org/img/wn/${
+                    forecastday.weather[0].icon
+                  }@2x.png"
                   alt=""
                   width="50"
                 />
                 <div class="weather-forecast-temperatures">
-                  <span class="weather-forecast-max">18°</span>
-                  <span class="weather-forecast-min">12°</span>
+                  <span class="weather-forecast-max">${Math.round(
+                    forecastday.temp.max
+                  )}°</span>
+                  <span class="weather-forecast-min">${Math.round(
+                    forecastday.temp.min
+                  )}°</span>
                 </div>
               </div>
   `;
+    }
   });
   forecastHTML = forecastHTML + `</div`;
   forecastElement.innerHTML = forecastHTML;
+}
+function getForecast(coordinates) {
+  let apiKey = "2daf65f0cdaa917f11026e8a128ce271";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function getCurrentTemperature(response) {
@@ -71,6 +94,8 @@ function getCurrentTemperature(response) {
     "alt",
     `http://openweathermap.org/img/wn/${response.data.weather[0].description}@2x.png`
   );
+
+  getForecast(response.data.coord);
 }
 
 function search(city) {
@@ -116,4 +141,3 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("New York");
-displayForecast();
