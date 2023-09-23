b9ea82f2f373a6078889ae320b216b13aed168fc
naledimuhauli <naledimuhauli@gmail.com>
2023-09-23 17:21:39 +0200

    removed the fahrenheit unit and changed the styling of the weather-app

--- src/app.js
+++ src/app.js
@@ -111,33 +111,9 @@ function handleSubmit(event) {
   search(cityInputElement.value);
 }
 
-function displayFehrenheitTemperature(event) {
-  event.preventDefault();
-
-  celsiusLink.classList.remove("active");
-  fahrenheitLink.classList.add("active");
-  let fahrenheitTemp = (celsiusTemp * 9) / 5 + 32;
-  let temperatureElement = document.querySelector("#temp");
-  temperatureElement.innerHTML = Math.round(fahrenheitTemp);
-}
-
-function displayCelsiusTemperature(event) {
-  event.preventDefault();
-  celsiusLink.classList.add("active");
-  fahrenheitLink.classList.remove("active");
-  let temperatureElement = document.querySelector("#temp");
-  temperatureElement.innerHTML = Math.round(celsiusTemp);
-}
-
 let celsiusTemp = null;
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
-
-let fahrenheitLink = document.querySelector("#fehrenheit-link");
-fahrenheitLink.addEventListener("click", displayFehrenheitTemperature);
-
-let celsiusLink = document.querySelector("#celsius-link");
-celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("New York");
